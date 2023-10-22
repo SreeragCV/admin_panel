e3721a7081b6684dc865cc1268c8c3cc87fef7df
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-22 16:03:35 +0530

    bcrypt added and tokens created

--- controller/dbcontroller.js
+++ controller/dbcontroller.js
@@ -1,20 +1,47 @@
 const pool = require("../config/dbconfig");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
 
 exports.createUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
     const userExist = await pool.query(`SELECT email FROM Users WHERE Users.email='${email}';`);
     if (!userExist.rows[0]) {
-      const newUser = await pool.query(`INSERT INTO Users (username, email, password) VALUES('${username}', '${email}', '${password}');`);
+      const newUser = await pool.query(`INSERT INTO Users (username, email, password) VALUES('${username}', '${email}', '${hashedPassword}');`);
       res.json(newUser.rows[0]);
-      console.log(newUser);
+      let payload = { username: username, email: email || 0 };
+      const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+      console.log(token);
+      res.status(200).json({token})
     } else {
-      res.status(401).json("this email or username is already registered");
+      res.status(200).json("THIS USERNAME OR PASSWORD HAS BEEN ALREADY REGISTERED");
     }
   } catch (err) {
     console.error(err.message);
   }
 };
+
+exports.loginUser = async (req, res) => {
+ try{ const {username, password, email} = req.body;
+  const oneUser = await pool.query(`SELECT * FROM Users WHERE username = '${username}';`);
+  const validPassword = await bcrypt.compare(password, oneUser.rows[0].password)
+  if(validPassword){
+    let payload = { username: username, email: email };
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+    return res.status(200).header("auth-token", token).send({ "token": token });
+  } else {
+    res.json('WRONG PASSWORD')
+  }
+}
+  catch(err){
+    console.log(err);
+  }
+}
+
+
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -42,7 +69,7 @@ exports.updateOneUser = async (req, res) => {
     const updatedNote = await pool.query(
       `UPDATE Users SET Username = ${username} WHERE note_id=${id};`
     );
-    res.json("User has been updated");
+    res.json("USER HAS BEEN UPDATED");
   } catch (err) {
     console.error(err.message);
   }
@@ -52,8 +79,10 @@ exports.deleteOneUser = async (req, res) => {
   try {
     const { id } = req.params;
     const deleteNote = await pool.query(`DELETE FROM Users WHERE id = ${id};`);
-    res.json("User has been deleted");
+    res.json("USER HAS BEEN DELETED");
   } catch (err) {
     console.error(err.message);
   }
 };
+
+
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const jwt = require("jsonwebtoken");
 const userController = require("./controller/dbcontroller");
 
 app.use(express.json())
 
 app.post("/create", userController.createUser)
+
+app.post("/login", userController.loginUser)
 
 app.get("/allusers", userController.getAllUsers)
 
@@ -15,16 +16,6 @@ app.put("/users/:id", userController.updateOneUser);
 
 app.delete("/users/:id", userController.deleteOneUser)
 
-
-app.post('/login', (req, res) => {
-    jwt.sign({user}, 'secret', {expiresIn: '300s'}, (err, token) => {
-        res.json(
-            {
-                token
-            }
-        )
-    })    
-})
 
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000");
